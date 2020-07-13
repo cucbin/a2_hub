451dc4d1b6600f4f3c6c616b5bd1ed8b1a390ed7
bill.shen <bill.shen@ringcentral.com>
2020-07-13 19:42:30 +0800

    remove unneed code.

--- app/src/main/java/io/ioprint/djinnidemo/DemoApplication.kt
+++ app/src/main/java/io/ioprint/djinnidemo/DemoApplication.kt
@@ -8,37 +8,13 @@ import android.os.Bundle
  * @author  bill.shen
  * <p>Date: 1/5/19</p>
  */
-class DemoApplication : Application(), Application.ActivityLifecycleCallbacks {
+class DemoApplication : Application() {
+
     init {
         System.loadLibrary("core")
     }
 
-    override fun onActivityPaused(activity: Activity?) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    override fun onCreate() {
+        super.onCreate()
     }
-
-    override fun onActivityResumed(activity: Activity?) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
-
-    override fun onActivityStarted(activity: Activity?) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
-
-    override fun onActivityDestroyed(activity: Activity?) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
-
-    override fun onActivitySaveInstanceState(activity: Activity?, outState: Bundle?) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
-
-    override fun onActivityStopped(activity: Activity?) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
-
-    override fun onActivityCreated(activity: Activity?, savedInstanceState: Bundle?) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
-
 }
